a9675a3dcc625dabd29a1a327da80039e8b1f5ec
Sejal Dahake <sejaldahake2001@gmail.com>
2024-02-16 18:47:05 +0530

    Add | Kotlin String Templates

---
+++ src/main/kotlin/06 String Templates/StringTemplate.kt
@@ -0,0 +1,22 @@
+package `06 String Templates`
+
+fun main(){
+
+    val emp = Employee("Sejal", 23)
+    println(emp)
+
+    val dollarBill = 100
+    println("I have $dollarBill bill")
+
+    // To avoid substitution
+    println("I have \$dollarBill")
+    println("I have $$dollarBill bill")
+
+    println("Employee ID is : ${emp.id}")
+}
+
+class Employee(var name: String, val id: Int){
+    override fun toString(): String {
+        return "Employee(name=$name, id=$id)"
+    }
+}
